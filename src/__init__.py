f19a0bcc9505af61c74b376992948f6c65324e8d
elJulioDev <alexisdevelopgz@gmail.com>
2026-07-28 13:41:30 -0400

    feat: update author information to use GITHUB_REPO_OWNER constant

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 """
 KeyForge - Herramienta de remapeo de teclas
 """
-from src.config.constants import CURRENT_VERSION
+from src.config.constants import CURRENT_VERSION, GITHUB_REPO_OWNER
 
 __version__ = CURRENT_VERSION
-__author__ = "elJulioDev"
+__author__ = GITHUB_REPO_OWNER
